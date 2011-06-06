3363740aa042fe22ec271f1b7436ad55b6b48006
Kerner <alex.kerner.24@googlemail.com>
2011-06-06 22:01:06 +0200

    JavaDoc.
    Change-Id: Ifab9350084743b0cd05832e11ec5fbe73fa8084d

--- src/main/java/net/sf/jtables/table/TableReader.java
+++ src/main/java/net/sf/jtables/table/TableReader.java
@@ -8,41 +8,43 @@ import net.sf.kerner.utils.io.buffered.IOIterable;
 
 /**
  * 
- * TODO description
+ * A {@code TableReader} reads an {@link AnnotatedTable} from an input source.
+ * </p> It does so by extending
+ * {@link net.sf.kerner.utils.io.buffered.IOIterable IOIterable} in oder to
+ * provide possibility to iterate over a table's rows.
+ * </p>
+ * Via {@link TableReader#readAll()} it is also possible, to read in a whole table at once.
  * 
- * <p>
- * <b>Example:</b><br>
- *
- * </p>
- * <p>
- * <pre>
- * TODO example
- * </pre>
- * </p>
- *
+ * @see IOIterable
+ * @see AnnotatedTable
+ * @see StringTable
+ * @see IntegerTable
+ * @see DoubleTable
+ * 
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
- * @version 2011-04-11
- *
- * @param <T>
+ * @version 2011-06-06
+ * 
+ * @param <T> type of elements in table
  */
-public interface TableReader<T> extends IOIterable<List<? extends T>>{
-	
+public interface TableReader<T> extends IOIterable<List<? extends T>> {
+
 	/**
 	 * Close this reader.
+	 * 
 	 * @see java.io.Reader#close()
-	 *
+	 * 
 	 */
 	void close();
-	
+
 	/**
 	 * 
 	 * Read a {@link Table} at once.
-	 *
+	 * 
 	 * @return
 	 * @throws IOException
 	 */
 	AnnotatedTable<T> readAll() throws IOException;
-	
+
 	/**
 	 * 
 	 */
