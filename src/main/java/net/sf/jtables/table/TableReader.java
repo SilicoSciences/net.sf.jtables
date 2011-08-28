980f1c725d9c935614d04e84d9ffe8b70a60cd28
Kerner <alex.kerner.24@googlemail.com>
2011-08-28 15:57:22 +0200

    [API-Break] TableReader: IOIterator<List<? extends T>> to IOIterator<List<T>>... not sure, hate generics!!

--- src/main/java/net/sf/jtables/table/TableReader.java
+++ src/main/java/net/sf/jtables/table/TableReader.java
@@ -1,9 +1,23 @@
+/**********************************************************************
+Copyright (c) 2009-2011 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.jtables.table;
 
 import java.io.IOException;
 import java.util.List;
 
-import net.sf.kerner.utils.io.buffered.AbstractIOIterator;
 import net.sf.kerner.utils.io.buffered.IOIterable;
 import net.sf.kerner.utils.io.buffered.IOIterator;
 
@@ -22,11 +36,11 @@ import net.sf.kerner.utils.io.buffered.IOIterator;
  * @see net.sf.jtables.table.impl.IntegerTable IntegerTable
  * 
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
- * @version 2011-06-06
+ * @version 2011-08-28
  * 
  * @param <T> type of elements in table
  */
-public interface TableReader<T> extends IOIterable<List<? extends T>> {
+public interface TableReader<T> extends IOIterable<List<T>> {
 
 	/**
 	 * Close this reader.
@@ -48,6 +62,6 @@ public interface TableReader<T> extends IOIterable<List<? extends T>> {
 	/**
 	 * 
 	 */
-	IOIterator<List<? extends T>> getIterator() throws IOException;
+	IOIterator<List<T>> getIterator() throws IOException;
 
 }
