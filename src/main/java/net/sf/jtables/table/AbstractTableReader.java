c90d5b25556aa59f6a4a22e4c87bb72df7edc71d
Kerner <alex.kerner.24@googlemail.com>
2012-01-10 17:13:13 +0100

    [Fix] StringTable/Reader: Fix column shift if table contains empty elements.

--- src/main/java/net/sf/jtables/table/AbstractTableReader.java
+++ src/main/java/net/sf/jtables/table/AbstractTableReader.java
@@ -1,3 +1,18 @@
+/**********************************************************************
+Copyright (c) 2009-2012 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.jtables.table;
 
 import java.io.File;
@@ -11,7 +26,6 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
-import net.sf.kerner.utils.StringUtils;
 import net.sf.kerner.utils.io.buffered.AbstractIOIterator;
 import net.sf.kerner.utils.io.buffered.IOIterator;
 
@@ -30,9 +44,9 @@ import net.sf.kerner.utils.io.buffered.IOIterator;
  * </p>
  *
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
- * @version 2011-04-11
+ * @version 2012-01-10
  *
- * @param <T>
+ * @param <T> type of elements
  */
 public abstract class AbstractTableReader<T> extends AbstractIOIterator<List<? extends T>>
 		implements TableReader<T> {
@@ -130,8 +144,8 @@ public abstract class AbstractTableReader<T> extends AbstractIOIterator<List<? e
 
 		while (scanner.hasNext()) {
 			final String s = scanner.next();
-			if(StringUtils.emptyString(s))
-				continue;
+//			if(StringUtils.emptyString(s))
+//				continue;
 			if (rowsB && first) {
 				rowids.add(s);
 				first = false;
