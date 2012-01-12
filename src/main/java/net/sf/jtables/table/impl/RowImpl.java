0a113509e117a4d3a7cf0d80fb84a37f403d0396
Kerner <alex.kerner.24@googlemail.com>
2012-01-12 16:54:46 +0100

    JavaDoc.

--- src/main/java/net/sf/jtables/table/impl/RowImpl.java
+++ src/main/java/net/sf/jtables/table/impl/RowImpl.java
@@ -1,3 +1,18 @@
+/**********************************************************************
+Copyright (c) 2011-2012 Alexander Kerner. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ ***********************************************************************/
+
 package net.sf.jtables.table.impl;
 
 import java.util.ArrayList;
@@ -15,41 +30,92 @@ import net.sf.kerner.utils.StringUtils;
 import net.sf.kerner.utils.collections.ObjectToIndexMapper;
 import net.sf.kerner.utils.collections.impl.ObjectToIndexMapperImpl;
 
+/**
+ * 
+ * Default implementation for {@link Row}.
+ * 
+ * <p>
+ * <b>Example:</b><br>
+ * 
+ * </p>
+ * <p>
+ * 
+ * <pre>
+ * TODO example
+ * </pre>
+ * 
+ * </p>
+ * 
+ * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
+ * @version 2012-01-12
+ * 
+ * @param <T>
+ *            type of table elements
+ */
 public class RowImpl<T> implements Row<T> {
 
+	/**
+	 * List implementation to which is delegated.
+	 */
 	protected final List<T> implementation = new ArrayList<T>();
-	
-	protected volatile ObjectToIndexMapper mapper = new ObjectToIndexMapperImpl(
-			new HashSet<Object>());
-	
+
+	/**
+	 * 
+	 */
+	protected volatile ObjectToIndexMapper mapper = new ObjectToIndexMapperImpl(new HashSet<Object>());
+
+	/**
+	 * 
+	 * Create a new Row that contains given elements.
+	 *
+	 * @param elements elements that are initially contained by this {@code Row}
+	 */
 	public RowImpl(List<T> elements) {
-		 implementation.addAll(elements);
+		implementation.addAll(elements);
 	}
-	
+
+	/**
+	 * 
+	 * Create an empty row.
+	 * 
+	 */
 	public RowImpl() {
 	}
 
+	// Implement //
+	
+	/**
+	 * 
+	 */
 	public void setIdentifier(Set<? extends Object> ids) {
 		this.mapper = new ObjectToIndexMapperImpl(ids);
 	}
-	
-	// Implement //
-	
+
+	/**
+	 * 
+	 */
 	public T get(Object indentifier) {
-//		if(implementation.size() <= mapper.get(indentifier)){
-//			throw new NoSuchElementException("no value for [" + indentifier + "]");
-//		}
+		// if(implementation.size() <= mapper.get(indentifier)){
+		// throw new NoSuchElementException("no value for [" + indentifier +
+		// "]");
+		// }
 		return get(mapper.get(indentifier));
 	}
 
+	/**
+	 * 
+	 */
 	public Set<Object> getIdentifier() {
 		return new LinkedHashSet<Object>(mapper.keySet());
 	}
-	
+
+	/**
+	 * 
+	 */
 	public ObjectToIndexMapper getObjectToIndexMapper() {
 		return new ObjectToIndexMapperImpl(mapper.keySet());
 	}
-	
+
 	// Delegates //
 
 	public int size() {
@@ -116,7 +182,7 @@ public class RowImpl<T> implements Row<T> {
 	public int hashCode() {
 		return implementation.hashCode();
 	}
-	
+
 	@Override
 	public String toString() {
 		return implementation.toString();
@@ -157,5 +223,5 @@ public class RowImpl<T> implements Row<T> {
 	public List<T> subList(int fromIndex, int toIndex) {
 		return implementation.subList(fromIndex, toIndex);
 	}
-	
+
 }
