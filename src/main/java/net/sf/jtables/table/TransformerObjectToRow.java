1d6c04136a09bf44036b463ba3327fc224d21f74
Kerner <alex.kerner.24@googlemail.com>
2012-03-12 10:50:41 +0100

    Feature: Introduce TransformerObjectToRow*.

---
+++ src/main/java/net/sf/jtables/table/TransformerObjectToRow.java
@@ -0,0 +1,7 @@
+package net.sf.jtables.table;
+
+import net.sf.kerner.utils.transformer.Transformer;
+
+public interface TransformerObjectToRow<V, T> extends Transformer<V, Row<T>>{
+
+}
---
+++ src/main/java/net/sf/jtables/table/TransformerObjectToStringRow.java
@@ -0,0 +1,5 @@
+package net.sf.jtables.table;
+
+public interface TransformerObjectToStringRow<V> extends TransformerObjectToRow<V, String> {
+
+}
