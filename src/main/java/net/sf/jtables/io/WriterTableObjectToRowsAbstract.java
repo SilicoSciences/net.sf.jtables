c5b117b3f1916864fb918bd84a727386cc574194
Kerner <alexanderkerner24@gmail.com>
2013-02-28 11:25:55 +0100

    CleanUp.

--- src/main/java/net/sf/jtables/io/WriterTableObjectToRowsAbstract.java
+++ src/main/java/net/sf/jtables/io/WriterTableObjectToRowsAbstract.java
@@ -6,43 +6,43 @@ import java.io.OutputStream;
 import java.io.Writer;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 public abstract class WriterTableObjectToRowsAbstract<T> extends WriterTableString {
-	
-	public WriterTableObjectToRowsAbstract(File file) throws IOException {
-		super(file);
-	}
 
-	public WriterTableObjectToRowsAbstract(OutputStream stream) {
-		super(stream);
-	}
+    public WriterTableObjectToRowsAbstract(final File file) throws IOException {
+        super(file);
+    }
 
-	public WriterTableObjectToRowsAbstract(Writer writer) {
-		super(writer);
-	}
+    public WriterTableObjectToRowsAbstract(final OutputStream stream) throws IOException {
+        super(stream);
+    }
 
-	public WriterTableObjectToRowsAbstract<T> writeElement(T element) throws IOException {
-		writeElement(DEFAULT_DELIMITER, element);
-		return this;
-	}
-	
-	public WriterTableObjectToRowsAbstract<T> writeElements(Collection<? extends T> elements) throws IOException {
-		writeElements(DEFAULT_DELIMITER, elements);
-		return this;
-	}
-	
-	public WriterTableObjectToRowsAbstract<T> writeElements(String delimiter, Collection<? extends T> elements) throws IOException {
-		Iterator<? extends T> it = elements.iterator();
-		while(it.hasNext()){
-			writeElement(delimiter, it.next());
-			if(it.hasNext()){
-				super.writer.newLine();
-			}
-		}
-		return this;
-	}
-	
-	public abstract WriterTableObjectToRowsAbstract<T> writeElement(String delimiter, T element) throws IOException;
+    public WriterTableObjectToRowsAbstract(final Writer writer) throws IOException {
+        super(writer);
+    }
+
+    public abstract WriterTableObjectToRowsAbstract<T> writeElement(String delimiter, T element) throws IOException;
+
+    public WriterTableObjectToRowsAbstract<T> writeElement(final T element) throws IOException {
+        writeElement(DEFAULT_DELIMITER, element);
+        return this;
+    }
+
+    public WriterTableObjectToRowsAbstract<T> writeElements(final Collection<? extends T> elements) throws IOException {
+        writeElements(DEFAULT_DELIMITER, elements);
+        return this;
+    }
+
+    public WriterTableObjectToRowsAbstract<T> writeElements(final String delimiter,
+            final Collection<? extends T> elements) throws IOException {
+        final Iterator<? extends T> it = elements.iterator();
+        while (it.hasNext()) {
+            writeElement(delimiter, it.next());
+            if (it.hasNext()) {
+                super.writer.newLine();
+            }
+        }
+        return this;
+    }
 
 }
