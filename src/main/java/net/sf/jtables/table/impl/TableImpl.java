31b9ed41b6310327e4c5efd00e7986449cfa8233
Kerner <alexanderkerner24@gmail.com>
2014-12-03 18:34:27 +0100

    Feature: Add template constructor.

--- src/main/java/net/sf/jtables/table/impl/TableImpl.java
+++ src/main/java/net/sf/jtables/table/impl/TableImpl.java
@@ -26,13 +26,13 @@ import net.sf.jtables.table.Table;
 import net.sf.kerner.utils.io.UtilIO;
 
 /**
- * 
+ *
  * Default implementation for {@link Table}.
- * 
- * 
+ *
+ *
  * @author <a href="mailto:alex.kerner.24@googlemail.com">Alexander Kerner</a>
  * @version 2012-01-22
- * 
+ *
  * @param <T>
  *            type of elements in this {@code Table}
  */
@@ -41,7 +41,7 @@ public class TableImpl<T> implements Table<T> {
     // Field //
 
     /**
-	 * 
+	 *
 	 */
     protected final List<Row<T>> rows = new ArrayList<Row<T>>();
 
@@ -50,28 +50,32 @@ public class TableImpl<T> implements Table<T> {
     public TableImpl() {
     }
 
-    public TableImpl(List<Row<T>> rows) {
+    public TableImpl(final List<Row<T>> rows) {
         synchronized (rows) {
             this.rows.addAll(rows);
         }
+    }
+
+    public TableImpl(final Table<T> template) {
+        this(template.getRows());
     }
 
     // Private //
 
     // Protected //
 
-    protected void checkRowIndex(int index) {
-        if (index < 0)
-            throw new IllegalArgumentException("row index must be >= 0");
-        if (index >= getNumberOfRows())
-            throw new NoSuchElementException("no element for row index [" + index + "]");
-    }
-
-    protected void checkColumnIndex(int index) {
+    protected void checkColumnIndex(final int index) {
         if (index < 0)
             throw new IllegalArgumentException("column index must be >= 0");
         if (index >= getNumberOfColumns())
             throw new NoSuchElementException("no element for column index [" + index + "]");
+    }
+
+    protected void checkRowIndex(final int index) {
+        if (index < 0)
+            throw new IllegalArgumentException("row index must be >= 0");
+        if (index >= getNumberOfRows())
+            throw new NoSuchElementException("no element for row index [" + index + "]");
     }
 
     // Public //
@@ -80,35 +84,22 @@ public class TableImpl<T> implements Table<T> {
 
     // Override //
 
-    @Override
-    public String toString() {
-        final StringBuilder sb = new StringBuilder();
-        final Iterator<?> it = getRowIterator();
-        while (it.hasNext()) {
-            sb.append(it.next());
-            if (it.hasNext())
-                sb.append(UtilIO.NEW_LINE_STRING);
-        }
-        return sb.toString();
+    /**
+	 *
+	 */
+    public boolean contains(final T element) {
+        return getAllElements().contains(element);
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((rows == null) ? 0 : rows.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
+    public boolean equals(final Object obj) {
         if (this == obj)
             return true;
         if (obj == null)
             return false;
         if (!(obj instanceof TableImpl))
             return false;
-        TableImpl<?> other = (TableImpl<?>) obj;
+        final TableImpl<?> other = (TableImpl<?>) obj;
         if (rows == null) {
             if (other.rows != null)
                 return false;
@@ -118,40 +109,38 @@ public class TableImpl<T> implements Table<T> {
     }
 
     /**
-     * Retrieve an iterator over all elements.
-     */
-    public synchronized Iterator<T> iterator() {
-        return getAllElements().iterator();
+	 *
+	 */
+    public T get(final int i, final int j) {
+        checkColumnIndex(j);
+        return getRow(i).get(j);
     }
 
     /**
-	 * 
+	 *
 	 */
-    public Row<T> getRow(int index) {
-        checkRowIndex(index);
-        // TODO defensive copying since we are immutable
-        return rows.get(index);
-    }
+    public List<T> getAllElements() {
+        final ArrayList<T> result = new ArrayList<T>();
 
-    /**
-	 * 
-	 */
-    public List<Row<T>> getRows() {
-        final List<Row<T>> result = new ArrayList<Row<T>>();
         for (int i = 0; i < getNumberOfRows(); i++) {
-            // TODO defensive copying since we are immutable
-            result.add(new RowImpl<T>(getRow(i)));
+
+            final List<T> row = getRow(i);
+            // defensive copying since we are immutable
+            for (final T t : row) {
+                // defensive copying since we are immutable
+                result.add(t);
+            }
         }
         return result;
     }
 
     /**
-	 * 
+	 *
 	 */
-    public Column<T> getColumn(int index) {
+    public Column<T> getColumn(final int index) {
         checkColumnIndex(index);
         final List<T> result = new ArrayList<T>();
-        for (List<? extends T> l : rows) {
+        for (final List<? extends T> l : rows) {
             if (index < l.size()) {
                 result.add(l.get(index));
             } else {
@@ -162,7 +151,14 @@ public class TableImpl<T> implements Table<T> {
     }
 
     /**
-	 * 
+	 *
+	 */
+    public Iterator<Column<T>> getColumnIterator() {
+        return new ArrayList<Column<T>>(getColumns()).iterator();
+    }
+
+    /**
+	 *
 	 */
     public List<Column<T>> getColumns() {
         final List<Column<T>> result = new ArrayList<Column<T>>();
@@ -174,45 +170,18 @@ public class TableImpl<T> implements Table<T> {
     }
 
     /**
-	 * 
+	 *
 	 */
-    public T get(int i, int j) {
-        checkColumnIndex(j);
-        return getRow(i).get(j);
-    }
-
-    /**
-	 * 
-	 */
-    public int getRowSize(int index) {
-        return getRow(index).size();
-    }
-
-    /**
-	 * 
-	 */
-    public int getColumnSize(int index) {
+    public int getColumnSize(final int index) {
         return getColumn(index).size();
     }
 
     /**
-	 * 
-	 */
-    public int getMaxRowSize() {
-        int result = 0;
-        for (List<?> r : rows) {
-            if (r.size() > result)
-                result = r.size();
-        }
-        return result;
-    }
-
-    /**
-	 * 
+	 *
 	 */
     public int getMaxColumnSize() {
         int result = 0;
-        for (List<? extends T> r : getColumns()) {
+        for (final List<? extends T> r : getColumns()) {
             if (r.size() > result)
                 result = r.size();
         }
@@ -220,56 +189,86 @@ public class TableImpl<T> implements Table<T> {
     }
 
     /**
-	 * 
+	 *
 	 */
-    public int getNumberOfRows() {
-        return rows.size();
+    public int getMaxRowSize() {
+        int result = 0;
+        for (final List<?> r : rows) {
+            if (r.size() > result)
+                result = r.size();
+        }
+        return result;
     }
 
     /**
-	 * 
+	 *
 	 */
     public int getNumberOfColumns() {
         return getMaxRowSize();
     }
 
     /**
-	 * 
+	 *
 	 */
-    public boolean contains(T element) {
-        return getAllElements().contains(element);
+    public int getNumberOfRows() {
+        return rows.size();
     }
 
     /**
-	 * 
+	 *
 	 */
-    public List<T> getAllElements() {
-        final ArrayList<T> result = new ArrayList<T>();
-
-        for (int i = 0; i < getNumberOfRows(); i++) {
-
-            final List<T> row = getRow(i);
-            // defensive copying since we are immutable
-            for (T t : row) {
-                // defensive copying since we are immutable
-                result.add(t);
-            }
-        }
-        return result;
+    public Row<T> getRow(final int index) {
+        checkRowIndex(index);
+        // TODO defensive copying since we are immutable
+        return rows.get(index);
     }
 
     /**
-	 * 
+	 *
 	 */
     public Iterator<Row<T>> getRowIterator() {
         return new ArrayList<Row<T>>(getRows()).iterator();
     }
 
     /**
-	 * 
+	 *
 	 */
-    public Iterator<Column<T>> getColumnIterator() {
-        return new ArrayList<Column<T>>(getColumns()).iterator();
+    public List<Row<T>> getRows() {
+        return rows;
+    }
+
+    /**
+	 *
+	 */
+    public int getRowSize(final int index) {
+        return getRow(index).size();
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((rows == null) ? 0 : rows.hashCode());
+        return result;
+    }
+
+    /**
+     * Retrieve an iterator over all elements.
+     */
+    public synchronized Iterator<T> iterator() {
+        return getAllElements().iterator();
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        final Iterator<?> it = getRowIterator();
+        while (it.hasNext()) {
+            sb.append(it.next());
+            if (it.hasNext())
+                sb.append(UtilIO.NEW_LINE_STRING);
+        }
+        return sb.toString();
     }
 
 }
